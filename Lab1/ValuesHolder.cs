ced4d9a1c9dd641d42481635b7d66b4ec8c69166
Алексей Афанасьев <75749232+AlexeyAGreyrat@users.noreply.github.com>
2021-03-24 16:47:53 +0300

    Add files via upload

---
+++ Lab1/ValuesHolder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class ValuesHolder
+    {
+        public List<WeatherForecast> Values { get; set; } = new List<WeatherForecast>();
+    }
+}
